3ea9bb5f780092c944a2c13522242f15b9a28072
tanisha <tanisha-email@example.com>
2025-07-02 12:48:06 +0530

    commit

---
+++ java.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class java{
+  public static void main(String[] args){
+    System.out.print("Enter your roll number: ");
+   Scanner sc = new Scanner(System.in);
+    int rollno = sc.nextInt();
+   
+	
+  }
+
+
+}
